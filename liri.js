fa03a613c57e9887bd3fc9182e86054d3a03ecac
Tyawo <48452575+Tyawo@users.noreply.github.com>
2019-04-24 18:02:17 -0400

    updated liri

--- liri.js
+++ liri.js
@@ -27,3 +27,60 @@ var bandsintown = (keys.bandsintown);
 var userInput = process.argv[2];
 var userQuery = process.argv.slice(3).join(" ");
 
+// APP LOGIC
+function userCommand(userInput, userQuery) {
+    switch (userInput) {
+        case "concert-this":
+            concertThis();
+            break;
+        case "spotify-this-song":
+            spotifyThisSong();
+            break;
+        case "movie-this":
+            movieThis();
+            break;
+        case "do-what-it-says":
+            doWhatItSays(userQuery);
+            break;
+        default:
+            console.log("I don't understand");
+            break;
+    }
+};
+userCommand(userInput, userQuery);
+
+function concertThis() {
+    console.log(`\n-----\n\nSEARCHING FOR ...${userQuery}'s next show...`);
+    // use request a our query URL using our user query variable as the parameters of our search
+    request("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=" + bandsintown, 
+    function(error,response,body) {
+
+        //  If there is no error
+        if (!error && response.statusCode === 200) {
+        // capture data and use JSON
+        var userBand = JSON.parse(body);
+        // parse data and use for loop to access paths of data
+        if (userBand.length > 0) {
+            for (i = 0; i < 5; i++) {
+
+                // console log desired data
+                console.log(`\nHERE ARE THE RESULTS...\n
+                            \nArtist: ${userBand[i].lineup[0]}
+                            \nVenue: ${userBand[i].venue.name}
+                            \nVenue Location: ${userBand[i].venue.latitude},${userBand[i].venue.longitude}
+                            \nVenue City: ${userBand[i].venue.city}, ${userBand[i].venue.country}`)
+
+                // Formatting the date with moment.js
+                var concertDate = moment(userBand[i].datetime).format("MM/DD/YYYY hh:00 A");
+                console.log(`\nDate and Time: ${concertDate}\n\n---------------------------------------------`);
+            };
+        } else {
+            console.log('Band or concert not found!');
+        };
+    };
+});
+}
+
+
+
+    
